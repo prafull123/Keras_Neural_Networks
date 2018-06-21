a31976a020753ddc58b7e097cb8d7e6e19c9c6bd
prafull123 <techprafull123@gmail.com>
2018-06-21 18:01:43 +0200

    Update ChurnModelling.py.py

--- BankChurnModellingNN/ChurnModelling.py.py
+++ BankChurnModellingNN/ChurnModelling.py.py
@@ -1,26 +1,28 @@
-# Artificial Neural Network
-
+# Artificial Neural Network application in Bank churn modelling
+# Libraries used and there installation - 
 # Installing Theano
 # pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
-
 # Installing Tensorflow
 # pip install tensorflow
-
 # Installing Keras
 # pip install --upgrade keras
-
-# Part 1 - Data Preprocessing
 
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
+#---------------------------------------------------------------------------------------------
+#                                   Data Preprocessing   
+#                                   Step 1 -  Data load
+#---------------------------------------------------------------------------------------------
 # Importing the dataset
 dataset = pd.read_csv('Churn_Modelling.csv')
 X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
-
+#---------------------------------------------------------------------------------------------
+#                                   Data Preprocessing   
+#                                   Step 2 - Handling Categorical Features
+#---------------------------------------------------------------------------------------------
 # Encoding categorical data
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X_1 = LabelEncoder()
@@ -30,19 +32,28 @@ X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
 onehotencoder = OneHotEncoder(categorical_features = [1])
 X = onehotencoder.fit_transform(X).toarray()
 X = X[:, 1:]
-
+#---------------------------------------------------------------------------------------------
+#                                   Data Preprocessing   
+#                                   Step 3 - Training and Test Split
+#---------------------------------------------------------------------------------------------
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-
+#---------------------------------------------------------------------------------------------
+#                                   Data Preprocessing   
+#                                   Step 4 - Standarisation ( Scaling )
+#---------------------------------------------------------------------------------------------
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# Part 2 - Now let's make the ANN!
 
+#---------------------------------------------------------------------------------------------
+#                                   Artificial Neural Network Building  
+#                                   Step 4 - Standarisation ( Scaling )
+#---------------------------------------------------------------------------------------------
 # Importing the Keras libraries and packages
 import keras
 from keras.models import Sequential
@@ -71,7 +82,10 @@ classifier.fit(X_train, y_train, batch_size = 10, epochs = 100)
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
 y_pred = (y_pred > 0.5)
-
+#---------------------------------------------------------------------------------------------
+#                                   Artificial Neural Network Building  
+#                                   Step 5 - Confusion Metrics for Evaluating model performance
+#---------------------------------------------------------------------------------------------
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
